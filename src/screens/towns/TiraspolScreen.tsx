bc223d60a544f501d2fc221538b1b1bbe1ef619b
horzzzz <tyuryuchkin@list.ru>
2024-05-28 13:51:48 +0300

    рабочий firestore

--- src/screens/towns/TiraspolScreen.tsx
+++ src/screens/towns/TiraspolScreen.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import { useEffect, useState } from 'react';
 
 
 const TiraspolScreen: React.FC = () => {
+
+  const [tir, setTir] = useState();
+
+  const getData = async () => {
+    const TiraspolDoc = await firestore().collection('Tiraspol').doc('0c0mjU07uQuPa7a4GH21').get();
+    console.log(TiraspolDoc.data())
+    setTir(TiraspolDoc.data())
+  }
+
+  useEffect(()=>{
+    getData()
+  }, [])
+
   return (
     <View>
       <Text>Hello Tiraspol!</Text>
+      <Text>{tir?.title}</Text>
     </View>
   );
 };
